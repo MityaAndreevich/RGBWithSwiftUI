caaaf8aabc092c91ed2dbed6ffb1c152f9e93602
MityaAndreevich <dmitry.logachev@icloud.com>
2021-10-29 19:58:31 +0300

    first slider made and view

--- RGBWithSwiftUI/ContentView.swift
+++ RGBWithSwiftUI/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redSliderValue = Double.random(in: 0...255)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image("Image")
+                .frame(width: 250, height: 150)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .background(Color.yellow)
+            RedSliderView(value: $redSliderValue, color: .red)
+            Spacer()
+        }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct RedSliderView: View {
+    @Binding var value: Double
+    let color: Color
+    
+    var body: some View {
+        HStack {
+            Text("\(value)")
+                .foregroundColor(color)
+            Slider(value: $value)
+            
+        }
     }
 }
