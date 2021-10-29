ecaa00554571da82e2f94be2a38dc9fbdbf1fe93
MityaAndreevich <dmitry.logachev@icloud.com>
2021-10-29 15:28:43 +0300

    Initial Commit

---
+++ RGBWithSwiftUI/RGBWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBWithSwiftUIApp.swift
+//  RGBWithSwiftUI
+//
+//  Created by Dmitry Logachev on 29.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct RGBWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
