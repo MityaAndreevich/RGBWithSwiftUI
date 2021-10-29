4e1e7bea6e9bbf0a3b25345a908c01e62d0e52df
MityaAndreevich <dmitry.logachev@icloud.com>
2021-10-29 20:43:09 +0300

    all sliders

--- RGBWithSwiftUI/ContentView.swift
+++ RGBWithSwiftUI/ContentView.swift
@@ -9,14 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var redTextField = ""
+    
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var greenTextField = ""
+    
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    @State private var blueTextField = ""
+    
+    @State private var viewColor = Color.yellow
     
     var body: some View {
         VStack {
-            Image("Image")
+            Image(uiImage: <#T##UIImage#>)
                 .frame(width: 250, height: 150)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .background(Color.yellow)
-            RedSliderView(value: $redSliderValue, color: .red)
+                .background(viewColor)
+            SliderView(value: $redSliderValue, text: $redTextField, color: .red)
+            SliderView(value: $greenSliderValue, text: $greenTextField, color: .green)
+            SliderView(value: $blueSliderValue, text: $blueTextField, color: .blue)
+            
             Spacer()
         }
         .padding()
@@ -29,16 +40,20 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct RedSliderView: View {
+struct SliderView: View {
     @Binding var value: Double
+    @Binding var text: String
     let color: Color
     
     var body: some View {
         HStack {
-            Text("\(value)")
+            Text("\(lround(value))")
                 .foregroundColor(color)
-            Slider(value: $value)
-            
+                .frame(width: 45)
+            Slider(value: $value, in: 0...255, step: 1)
+            TextField("\(lround(value))", text: $text)
+                .frame(width: 45)
+                .textFieldStyle(.roundedBorder)
         }
     }
 }
