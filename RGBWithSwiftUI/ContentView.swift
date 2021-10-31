14f358c6c6de33168f2578547bf9de0be227efc4
MityaAndreevich <dmitry.logachev@icloud.com>
2021-10-31 23:15:29 +0300

    not done yet

--- RGBWithSwiftUI/ContentView.swift
+++ RGBWithSwiftUI/ContentView.swift
@@ -17,19 +17,18 @@ struct ContentView: View {
     @State private var blueSliderValue = Double.random(in: 0...255)
     @State private var blueTextField = ""
     
-    @State private var alertPresenter = false
+    //@State private var alertPresenter = false
     
     var body: some View {
         ZStack {
             Color.gray
                 .ignoresSafeArea()
-            VStack(spacing: 30) {
-                Rectangle()
-                    .foregroundColor(Color(
-                        red: redSliderValue / 255,
-                        green: greenSliderValue / 255,
-                        blue: blueSliderValue / 255
-                    ))
+            VStack(spacing: 25) {
+                Color(
+                    red: redSliderValue / 255,
+                    green: greenSliderValue / 255,
+                    blue: blueSliderValue / 255
+                )
                     .frame(width: 250, height: 150)
                     .overlay(Rectangle().stroke(.white, lineWidth: 3))
                 SliderView(value: $redSliderValue, text: $redTextField, tint: .red)
@@ -40,11 +39,6 @@ struct ContentView: View {
             .padding()
         }
     }
-    
-    private func checkInputData() {
-        guard let _ = Double(redTextField) else { alertPresenter = true; return }
-        UIApplication.shared.endEditing()
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -54,6 +48,7 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct SliderView: View {
+    @State private var alertPresenter = false
     @Binding var value: Double
     @Binding var text: String
     let tint: Color
@@ -70,21 +65,27 @@ struct SliderView: View {
                 .keyboardType(.decimalPad)
                 .toolbar {
                     ToolbarItemGroup(placement: .keyboard) {
-                        Button("Done") {
-                            setValuesFromText()
-                            UIApplication.shared.endEditing()
-                        }
+                        Button("Done", action: setValuesFromText)
+                            .alert("Wrong Input", isPresented: $alertPresenter, actions: {})
                     }
                 }
         }
     }
     private func setValuesFromText() {
-        value = Double(text) ?? 0
+        if value >= 0 && value <= 255 {
+            value = Double(text) ?? 0
+            UIApplication.shared.endEditing()
+        } else {
+            alertPresenter.toggle()
+        }
     }
 }
+
+
 extension UIApplication {
     func endEditing() {
         sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
+
 
